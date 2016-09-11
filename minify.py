dff74c60f2def5105ad7970484209194aa4a44d6
Alvin Grant <agrant@besentient.com>
2016-09-10 23:11:19 -0400

    Complete revamp of minification functionality
    Use OOP principles to increase reusability and portability into legacy and future projects
    
    Develop two modes to either minify a single file or a group of files within a specified directory
    
    Update function instantiation to create object then call method to begin minification

--- minify.py
+++ minify.py
@@ -1,20 +1,50 @@
 import sys
 import re
+import os
 
 #Global variables for input html files
-file = sys.argv[1]
+mode = sys.argv[1]
+arg = sys.argv[2]
 
-def minify(file):	
-	#Read in file and remove line breaks and tabs
-	with open(file, 'r+') as i:
-		lines = i.read()
-		lines = re.sub("\n", "", lines)
-		lines = re.sub("	", "", lines)
-		i.seek(0)
-		i.write(lines)
-		i.truncate()
-	i.closed
+class Minify:
+	def __init__(self):
+		print "Minification started!"
+
+	def mode(self, mode, arg):
+		if mode == 'd':
+			self.minify_directory(arg)
+		elif mode == 'f':
+			self.minify_file(arg)
+		else:
+			print "Please specify mode"
+		return
+
+	def minify_directory(self, directory):	
+		#Read in files and remove line breaks and tabs
+		for f in os.listdir(directory):
+			file = directory + '/' + f
+			with open(file, 'r+') as i:
+				lines = i.read()
+				lines = re.sub("\n", "", lines)
+				lines = re.sub("	", "", lines)
+				i.seek(0)
+				i.write(lines)
+				i.truncate()
+			i.closed
+		print 'File directory minification complete!'
+
+	def minify_file(self, file):
+		with open(file, 'r+') as i:
+			lines = i.read()
+			lines = re.sub("\n", "", lines)
+			lines = re.sub("	", "", lines)
+			i.seek(0)
+			i.write(lines)
+			i.truncate()
+		i.closed
+		print 'File minification complete!'
 
 if __name__ == '__main__':
 	#Run Function
-	minify(file)
+	m = Minify()
+	m.mode(mode, arg)
