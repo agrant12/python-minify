b2805aecd5a739238b7e88806c23c382d229ea83
Alvin Grant <agrant@besentient.com>
2016-09-11 10:52:58 -0400

    Update Readme to include instructions for single file and directory magnification
    Add custom error functionality
    
    Update argument naming conventions

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
-HTML Minify
-========
+Frontend Minifification
+==============
 
-Simple python program that minifies html files for faster browser processing
+Python class/program that minifies html, js, and css files for faster front end application performance. Program is able to either minify a single file or a group of files within a directory.
+
+Simply reference the minify.py in terminal with python, pass in mode, followed by your directory/file path (detailed below).
 
 Initialization
 --------------
-In terminal run: 
-  python minify.py input.html
+
+In terminal:
+
+	For directory path:
+  		python minify.py d path/to/directory
+
+  	For single file path:
+  		python minify.py f path/to/file/file.extension 
--- minify.py
+++ minify.py
@@ -1,28 +1,43 @@
-import sys
-import re
-import os
+import sys, re, os
 
-#Global variables for input html files
+#Global mode and directory/file path
 mode = sys.argv[1]
-arg = sys.argv[2]
+path = sys.argv[2]
 
 class Minify:
 	def __init__(self):
 		print "Minification started!"
 
-	def mode(self, mode, arg):
+	def mode(self, mode, path):
 		if mode == 'd':
-			self.minify_directory(arg)
+			self.minify_directory(path)
 		elif mode == 'f':
-			self.minify_file(arg)
+			self.minify_file(path)
 		else:
-			print "Please specify mode"
+			return self.error("Incorrect mode param! Please use 'd' for directory minification or 'f' for single file minification.")
 		return
 
 	def minify_directory(self, directory):	
-		#Read in files and remove line breaks and tabs
-		for f in os.listdir(directory):
-			file = directory + '/' + f
+		#Read in files within directory and remove line breaks and tabs
+		if os.path.isdir(directory):
+			for f in os.listdir(directory):
+				file = directory + '/' + f
+				with open(file, 'r+') as i:
+					lines = i.read()
+					lines = re.sub("\n", "", lines)
+					lines = re.sub("	", "", lines)
+					i.seek(0)
+					i.write(lines)
+					i.truncate()
+				i.closed
+		else:
+			return self.error('Not a directory! Please enter directory path.')
+		
+		print 'File directory minification complete!'
+
+	def minify_file(self, file):
+		#Read in file and remove line breaks and tabs
+		if os.path.isfile(file):
 			with open(file, 'r+') as i:
 				lines = i.read()
 				lines = re.sub("\n", "", lines)
@@ -31,20 +46,16 @@ class Minify:
 				i.write(lines)
 				i.truncate()
 			i.closed
-		print 'File directory minification complete!'
-
-	def minify_file(self, file):
-		with open(file, 'r+') as i:
-			lines = i.read()
-			lines = re.sub("\n", "", lines)
-			lines = re.sub("	", "", lines)
-			i.seek(0)
-			i.write(lines)
-			i.truncate()
-		i.closed
+		else:
+			return self.error('Not a file! Please enter a file path.')
+		
 		print 'File minification complete!'
+
+	def error(self, err):
+		#Print error message
+		print err
 
 if __name__ == '__main__':
 	#Run Function
 	m = Minify()
-	m.mode(mode, arg)
+	m.mode(mode, path)
